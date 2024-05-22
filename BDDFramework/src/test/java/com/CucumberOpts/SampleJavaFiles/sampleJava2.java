a8c0b6a19236ff003f56904eddc08116b285c912
2172261 <2172261@LTIN377195.cts.com>
2024-05-22 18:38:50 +0530

    git commit file

---
+++ BDDFramework/src/test/java/com/CucumberOpts/SampleJavaFiles/sampleJava2.java
@@ -0,0 +1,18 @@
+package com.CucumberOpts.SampleJavaFiles;
+
+import java.text.SimpleDateFormat;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class sampleJava2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+		System.out.println(sdf.format(new java.util.Date()));
+		System.out.println(" string: "+sdf.toString());
+
+	}
+
+}
